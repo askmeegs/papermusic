226c1f2614c3187f93b01f803a1f1dd981d7142d
Megan O'Keefe <mokeefe@google.com>
2024-06-10 15:47:53 -0400

    save

--- gce/server.py
+++ gce/server.py
@@ -112,9 +112,9 @@ def listen():
 if __name__ == "__main__":
     # listen()
     for i in range(0, 50):
-        s = time.now()
+        s = time.time()
         res = inference_paligemma(
             "identify the musical instrument.", "framecapture/frame_40.jpg"
         )
-        f = time.now()
+        f = time.time()
         print(f"🔍 Identified instrument: {res} in {f-s} seconds")
